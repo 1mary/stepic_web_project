43cd5014f6ee3c6724cfae4540a877d2b4031c59
1mary <tyuleneva-1998@mail.ru>
2021-06-28 19:47:50 +0300

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,4 +1,4 @@
-urlpatterns = patterns('qa.views',                                              
+"""urlpatterns = patterns('qa.views',                                              
    url(r'^$', 'test'),                                                              
    url(r'^login/.*$', 'test', name='login'),                                    
    url(r'^signup/.*', 'test', name='signup'),                                   
@@ -6,4 +6,25 @@ urlpatterns = patterns('qa.views',
    url(r'^ask/.*', 'test', name='ask'),                                         
    url(r'^popular/.*', 'test', name='popular'),                                 
    url(r'^new/.*', 'test', name='new'),                                         
-) 
+) """
+"""ask URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from qa.views import question
+
+urlpatterns = [
+    url(r'^(?P<num>\d+)/$', question),
+]
